b272baf1da857f7541d68541785413bc87bcf9eb
konrad schlatte <konradschlatte@konrads-MacBook-Air.local>
2018-10-30 11:35:10 +0000

    added new mock test

--- tests/testIban.test.js
+++ tests/testIban.test.js
@@ -1,14 +1,23 @@
-const checker = require('../public/javascripts/checker')
+const Checker = require('../public/javascripts/checker')
 const stringCleaner = require('../public/javascripts/stringCleaner')
 const testIban = require('../public/javascripts/testIban')
 
 jest.mock('../public/javascripts/checker');
 
 beforeEach(() => {
-  checker.mockClear();
+  Checker.mockClear();
 });
 
 it('We can check if testIban called the class constructor', () => {
-  testIban(1);
-  expect(checker).toHaveBeenCalledTimes(1);
+  testIban('BE71 0961 2345 6769');
+  expect(Checker).toHaveBeenCalledTimes(1);
+});
+
+testIban('BE71 0961 2345 6769');
+let mockCheckerInstance = Checker.mock.instances[0];
+let mockCheckerInstanceCheck = mockCheckerInstance.check;
+console.log(mockCheckerInstanceCheck);
+it('We can check if the check method has been called in the constructor', () => {
+  testIban('BE71 0961 2345 6769');
+  expect(mockCheckerInstanceCheck).toHaveBeenCalledTimes(1);
 });
